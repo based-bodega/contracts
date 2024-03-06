fb44389e7631477d46c740d9ce34ea276bbe5765
Ryan Carter <tomhiddle03@gmail.com>
2024-03-06 14:14:43 +0900

    add deploy scripts

---
+++ scripts/deployRaffleFactory.js
@@ -0,0 +1,18 @@
+// This is script to deploy testnet erc20 Token. So no need for real project.
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const token = await ethers.deployContract("ContractFactory");
+  
+    console.log("Token address:", await token.getAddress());
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+  
