3376528f2d9c82534d6464cd38c8ebd013fec904
Ryan Carter <tomhiddle03@gmail.com>
2024-03-08 00:26:26 +0900

    add contract address and abis

---
+++ constants/contracts.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: {
+    84532: "0xa1eA90f4eE621f763B04ff76307424c453A72a57",
+  },
+  raffleFacotry: {
+    84532: "0xe5246fcF5D6FA6355bdF19F82a631349b08bfce3",
+  },
+};
