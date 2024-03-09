6bf2b710dc7c9c82b0f6ae541443a3a96410093e
Ryan Carter <tomhiddle03@gmail.com>
2024-03-09 20:03:34 +0900

    fix spelling issue

--- constants/contracts.js
+++ constants/contracts.js
@@ -2,7 +2,10 @@ module.exports = {
   token: {
     84532: "0xa1eA90f4eE621f763B04ff76307424c453A72a57",
   },
-  raffleFacotry: {
+  raffleFactory: {
+    84532: "0xe5246fcF5D6FA6355bdF19F82a631349b08bfce3",
+  },
+  stake: {
     84532: "0xe5246fcF5D6FA6355bdF19F82a631349b08bfce3",
   },
 };
